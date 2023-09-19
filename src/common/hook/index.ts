5f213dc4bbad38573bf6c04fc6599b4552774034
Siyeop <siyeop.kim@gmail.com>
2023-09-19 11:32:50 +0900

    add export target

--- src/common/hook/index.ts
+++ src/common/hook/index.ts
@@ -1,5 +1,6 @@
-export {default as useClickOutside} from './UseClickOutside'
-export {default as useInputState} from './UseInputState'
-export {default as useRipple} from './UseRipple'
-export {default as useValidator} from './UseValidator'
-export {default as usePagination} from './UsePagination'
+export {default as useClickOutside} from './UseClickOutside';
+export {default as useInputState} from './UseInputState';
+export {default as useRipple} from './UseRipple';
+export {default as useValidator} from './UseValidator';
+export {default as usePagination} from './UsePagination';
+export {default as useRefs} from './UseRefs';
