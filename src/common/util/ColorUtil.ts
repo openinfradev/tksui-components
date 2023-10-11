e2569e2fc501cb6b2b2b0ec5cacdae9bd4cf60d8
Siyeop <siyeop.kim@gmail.com>
2023-10-11 10:19:39 +0900

    extract primary color as css variables

--- src/common/util/ColorUtil.ts
+++ src/common/util/ColorUtil.ts
@@ -1,16 +1,18 @@
 
 function shadeColor(baseColor, amount): string {
-    const baseColorRGB = baseColor.match(/\d+/g);
+
+    const baseColorRGB = baseColor.match(/\d+(\.\d+)?/g);
     const shadedColor = [
         Math.max(0, parseInt(baseColorRGB[0], 10) - amount),
         Math.max(0, parseInt(baseColorRGB[1], 10) - amount),
         Math.max(0, parseInt(baseColorRGB[2], 10) - amount),
     ];
+
     return `rgb(${shadedColor.join(',')})`;
 }
 
 function tintColor(baseColor, amount) {
-    const baseColorRGB = baseColor.match(/\d+/g);
+    const baseColorRGB = baseColor.match(/\d+(\.\d+)?/g);
     const tintedColor = [
         Math.min(255, parseInt(baseColorRGB[0], 10) + amount),
         Math.min(255, parseInt(baseColorRGB[1], 10) + amount),
