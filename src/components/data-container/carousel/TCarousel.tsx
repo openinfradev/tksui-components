c6523d8a9520ae4c21383f07daf89209062ad649
Siyeop <siyeop.kim@gmail.com>
2023-11-01 15:47:55 +0900

    bugfix

--- src/components/data-container/carousel/TCarousel.tsx
+++ src/components/data-container/carousel/TCarousel.tsx
@@ -1,5 +1,5 @@
 import {CSSProperties, useCallback, useMemo, useRef} from 'react';
-import Slider from 'react-slick/';
+import Slider from 'react-slick';
 import {TCarouselProps, TIconButton} from '@/components';
 
 const TCarousel = (props: TCarouselProps) => {
