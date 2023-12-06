cf5616b6546151f5de24cc92e59c89302f997a83
Kokwangnam <psychiblue@strato.co.kr>
2023-12-06 10:48:46 +0900

    bugfix. Modify meta

--- stories/components/data-container/card/TCard.stories.tsx
+++ stories/components/data-container/card/TCard.stories.tsx
@@ -1,20 +1,19 @@
 import {Meta, StoryObj} from '@storybook/react';
 
 import React, {useState} from 'react';
-import TStepBox from '~/data-container/step-box/TStepBox';
 import TCard from '~/data-container/card/TCard';
 import TCardHeader from '~/data-container/card/TCardHeader';
 import TCardContent from '~/data-container/card/TCardContent';
 
 
-const meta: Meta<typeof TStepBox> = {
+const meta: Meta<typeof TCard> = {
     title: 'DataContainer/TCard',
-    component: TStepBox,
+    component: TCard,
 };
 
 export default meta;
 
-type Story = StoryObj<typeof TStepBox>;
+type Story = StoryObj<typeof TCard>;
 
 
 const Template = (args) => {
@@ -25,8 +24,8 @@ const Template = (args) => {
 
         <TCard {...args} clickable onClick={() => setVal('1')} width={'300px'} selected={val === '1'}>
             <TCardHeader title={'AWS'} subTitle={'Lorem ipsum dolor sit'}></TCardHeader>
-            {/* <CardDivider /> */}
-            <TCardContent>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor</TCardContent>
+        {/* <CardDivider /> */}
+        <TCardContent>Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor</TCardContent>
         </TCard>
 
         <TCard clickable onClick={() => setVal('2')} width={'300px'} selected={val === '2'}>
