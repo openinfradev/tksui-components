29928a5c378273ad03d93b7ec507cade63c3e9ee
Siyeop <Siyeop@users.noreply.github.com>
2024-01-19 14:08:43 +0000

    deploy: e279fb2820aedaf7f285c192b7911ec2b57ecb2d

---
+++ assets/TIcon.stories-cca07b23.js
@@ -0,0 +1,41 @@
+import{j as e}from"./jsx-runtime-ffb262ed.js";import{T as M}from"./TIcon-48c53834.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";const J={title:"Input/TIcon",component:M},G=a=>e.jsxs("div",{style:{display:"flex",flexDirection:"column",fontSize:"14px",gap:"8px",alignItems:"center"},children:[a.children,a.children.props.children]}),r=a=>{const z=["t_check_on","t_check_off","t_check_disabled_off","t_check_intermediate","t_radio_on","t_radio_off","t_radio_disabled_off","t_navigate_left","t_navigate_left_double","t_navigate_right","t_navigate_right_double","t_list","t_information","t_done","t_person","t_double_quotes_open","t_double_quotes_close"];return e.jsxs(e.Fragment,{children:[e.jsx("div",{children:e.jsx("a",{href:"https://fonts.google.com/icons",target:"_blank",style:{textDecoration:"underline",color:"blue",cursor:"pointer"},rel:"noreferrer",children:"아이콘 라이브러리 링크"})}),e.jsx("div",{style:{display:"grid",gridTemplateColumns:"repeat(auto-fill, 150px)",marginTop:"32px",rowGap:"40px"},children:z.map(m=>e.jsx(G,{children:e.jsx(M,{...a,children:m})},m))})]})},s={render:r,args:{type:"outlined"}},t={render:r,args:{type:"filled"}},o={render:r,args:{xsmall:!0}},n={render:r,args:{small:!0}},l={render:r,args:{medium:!0}},c={render:r,args:{large:!0}},d={render:r,args:{xlarge:!0}},i={render:r,args:{color:"#3617CE"}};var p,u,g;s.parameters={...s.parameters,docs:{...(p=s.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    type: 'outlined'
+  }
+}`,...(g=(u=s.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var _,f,x;t.parameters={...t.parameters,docs:{...(_=t.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    type: 'filled'
+  }
+}`,...(x=(f=t.parameters)==null?void 0:f.docs)==null?void 0:x.source}}};var h,S,T;o.parameters={...o.parameters,docs:{...(h=o.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    xsmall: true
+  }
+}`,...(T=(S=o.parameters)==null?void 0:S.docs)==null?void 0:T.source}}};var j,y,b;n.parameters={...n.parameters,docs:{...(j=n.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    small: true
+  }
+}`,...(b=(y=n.parameters)==null?void 0:y.docs)==null?void 0:b.source}}};var v,k,C;l.parameters={...l.parameters,docs:{...(v=l.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    medium: true
+  }
+}`,...(C=(k=l.parameters)==null?void 0:k.docs)==null?void 0:C.source}}};var I,E,L;c.parameters={...c.parameters,docs:{...(I=c.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    large: true
+  }
+}`,...(L=(E=c.parameters)==null?void 0:E.docs)==null?void 0:L.source}}};var X,F,O;d.parameters={...d.parameters,docs:{...(X=d.parameters)==null?void 0:X.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    xlarge: true
+  }
+}`,...(O=(F=d.parameters)==null?void 0:F.docs)==null?void 0:O.source}}};var q,w,D;i.parameters={...i.parameters,docs:{...(q=i.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    color: '#3617CE'
+  }
+}`,...(D=(w=i.parameters)==null?void 0:w.docs)==null?void 0:D.source}}};const K=["Outline","Filled","XSmall","Small","Medium","Large","XLarge","Colored"];export{i as Colored,t as Filled,c as Large,l as Medium,s as Outline,n as Small,d as XLarge,o as XSmall,K as __namedExportsOrder,J as default};
---
+++ assets/TModal.stories-6cb9efbc.js
@@ -0,0 +1,26 @@
+import{j as r}from"./jsx-runtime-ffb262ed.js";import{r as w}from"./index-76fb7be0.js";import{T as k}from"./TModal-7c60fe9a.js";import{T as c}from"./TButton-663e9d6d.js";import"./_commonjsHelpers-de833af9.js";import"./index-d3ea75b5.js";import"./index-9d475cdf.js";import"./TIcon-48c53834.js";import"./UseRipple-565834b6.js";const z={title:"Screen/TModal",component:k,args:{containerId:"storybook-root"}},e=C=>{const[E,m]=w.useState(!1),O=r.jsxs(r.Fragment,{children:[r.jsx(c,{width:"50%",onClick:()=>m(!1),children:"취소"}),r.jsx(c,{width:"50%",main:!0,children:"저장"})]});return r.jsxs(r.Fragment,{children:[r.jsx(c,{onClick:()=>m(!0),children:"모달 열기"}),r.jsx(k,{...C,title:"클러스터 생성",isOpen:E,onRequestClose:()=>m(!1),footer:O})]})},a={render:e,args:{small:!0}},s={render:e,args:{medium:!0}},t={render:e,args:{large:!0}},o={render:e,args:{xlarge:!0}},n={render:e,args:{xxlarge:!0}};var d,p,l;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    small: true
+  }
+}`,...(l=(p=a.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var i,u,g;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    medium: true
+  }
+}`,...(g=(u=s.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var x,T,f;t.parameters={...t.parameters,docs:{...(x=t.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    large: true
+  }
+}`,...(f=(T=t.parameters)==null?void 0:T.docs)==null?void 0:f.source}}};var S,j,h;o.parameters={...o.parameters,docs:{...(S=o.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    xlarge: true
+  }
+}`,...(h=(j=o.parameters)==null?void 0:j.docs)==null?void 0:h.source}}};var L,X,M;n.parameters={...n.parameters,docs:{...(L=n.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    xxlarge: true
+  }
+}`,...(M=(X=n.parameters)==null?void 0:X.docs)==null?void 0:M.source}}};const A=["Small","Medium","Large","XLarge","XXLarge"];export{t as Large,s as Medium,a as Small,o as XLarge,n as XXLarge,A as __namedExportsOrder,z as default};
