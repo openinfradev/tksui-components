86a3b4c7d4c494a0c3bc37267ab942fba1e9abfa
KailKo <psychiblue@strato.co.kr>
2024-02-06 13:13:02 +0900

    feature. Implement tooltip title #2

--- src/common/util/TooltipUtil.ts
+++ src/common/util/TooltipUtil.ts
@@ -15,10 +15,14 @@ function convertToTooltipAttributes(props: TBaseTooltipProps) {
     ret['data-tooltip-place'] = props.tooltipPlace;
     ret['data-tooltip-hidden'] = props.tooltipHidden;
 
-    try {
-        ret['data-tooltip-html'] = generateTooltipHtml(props);
-    } catch (error) {
-        ret['data-tooltip-html'] = '';
+    if (props.tooltipHtml) {
+        ret['data-tooltip-html'] = props.tooltipHtml;
+    } else {
+        try {
+            ret['data-tooltip-html'] = generateTooltipHtml(props);
+        } catch (e) {
+            ret['data-tooltip-html'] = '';
+        }
     }
 
     return ret;
