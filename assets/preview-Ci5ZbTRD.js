2f6519c59e9584eca20483b32a99576989ee6c62
Siyeop <Siyeop@users.noreply.github.com>
2024-02-15 07:03:05 +0000

    deploy: 4c83a7957baa4722f7aca68976f17f4ef40fde76

--- assets/preview-Ci5ZbTRD.js
+++
@@ -1,7 +0,0 @@
-import{_ as t}from"./iframe-Dis7Yiwf.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-D10twAnW.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{_ as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-D10twAnW.js","./iframe-Dis7Yiwf.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-Bw8VTzHM.js","./toString-Biug5Hwg.js","./_getPrototype-BHeJXxZI.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
