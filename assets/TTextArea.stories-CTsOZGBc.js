6d5b772b564a867df04bf13f577cd6e90b9a6fcf
Siyeop <Siyeop@users.noreply.github.com>
2024-03-08 01:37:29 +0000

    deploy: 8ca66c938c51d466a08fc8a3116d9a3f80b4838c

---
+++ assets/TTextArea.stories-CTsOZGBc.js
@@ -0,0 +1,25 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{r as N}from"./index-CBqU2yxZ.js";import{r as n}from"./TValidatorRule-BwpR5Vl5.js";import{u as l}from"./UseInputState-BMLhFmM_.js";import{T as E}from"./TButton-BGqwA6vn.js";import{u as F}from"./UseRefs-B71Mt8Xu.js";import{T as t}from"./TTextArea-CvuaTDHk.js";import"./_commonjsHelpers-BosuxZz1.js";import"./UseRipple-B0f24SXl.js";import"./uniqueId-BdmaqBQH.js";import"./toString-Biug5Hwg.js";import"./TIcon-xlMQuwvA.js";import"./UseValidator-XS2alT9g.js";const $={title:"Input/TTextArea",component:t},z=r=>{const[a,s]=N.useState(""),c={width:"500px",border:"1px solid lightgray",padding:"16px",marginTop:"16px",display:"flex",flexDirection:"column",gap:"16px"};return e.jsx(e.Fragment,{children:e.jsx("div",{style:{display:"flex",gap:"24px"},children:e.jsxs("div",{children:[e.jsx("span",{style:{fontSize:"20px"},children:"Normal, 일반 웹페이지"}),e.jsxs("div",{style:c,children:[e.jsx(t,{...r,value:a,onChange:s,label:"무옵션"}),e.jsx(t,{...r,value:a,onChange:s,label:"Placeholder",placeholder:"설명을 입력해주세요"}),e.jsx(t,{...r,value:a,onChange:s,label:"Counter",counter:200}),e.jsx(t,{...r,value:a,onChange:s,label:"Read-only",disabled:!0}),e.jsx(t,{...r,value:a,onChange:s,label:"너비-400",width:"400px"}),e.jsx(t,{...r,value:a,onChange:s,label:"너비-300",width:"300px"}),e.jsx(t,{...r,value:a,onChange:s,label:"너비-200",width:"200px"})]})]})})})},o={render:z,args:{type:"outline",rows:4}},i={render:z,args:{type:"underline",rows:4}},B=r=>{const a=l(""),s=l(""),c=l(""),M=l(""),O=l(""),U=l(""),[p,x,m,g,h,f]=F(6),D={width:"500px",border:"1px solid lightgray",padding:"16px",marginTop:"16px",display:"flex",flexDirection:"column",gap:"16px"},L=()=>{p.current.validate(),x.current.validate(),m.current.validate(),g.current.validate(),h.current.validate(),f.current.validate()};return e.jsxs(e.Fragment,{children:[e.jsx(E,{main:!0,onClick:L,children:"검사"}),e.jsx("div",{style:{display:"flex",gap:"24px"},children:e.jsxs("div",{style:D,children:[e.jsx(t,{...r,label:"Outline Default",rules:[n.required(),n.lengthBetween(3,12)],...a,ref:p,placeholder:"값을 입력해 주세요",counter:12}),e.jsx(t,{...r,label:"Outline Custom Long Message",rules:[n.required("에러 메시지가 길면 벽에 닿아서 말 줄임표로 표현됩니다. 에러 메시지가 길면 벽에 닿아서 말 줄임표로 표현됩니다."),n.lengthBetween(3,12)],...s,ref:x,counter:12}),e.jsx(t,{...r,label:"Outline Success Message",rules:[n.required(),n.lengthBetween(3,12)],counter:12,...c,ref:m,successMessage:"사용할 수 있는 아이디입니다"}),e.jsx(t,{...r,label:"Underline Default",rules:[n.required(),n.lengthBetween(3,12)],counter:12,...M,ref:g,type:"underline"}),e.jsx(t,{...r,label:"Underline Custom Long Message",rules:[n.required("에러 메시지가 길면 벽에 닿아서 말 줄임표로 표현됩니다. 에러 메시지가 길면 벽에 닿아서 말 줄임표로 표현됩니다."),n.lengthBetween(3,12)],counter:12,...O,ref:h,type:"underline"}),e.jsx(t,{...r,label:"Underline Success Message",rules:[n.required(),n.lengthBetween(3,12)],counter:12,successMessage:"사용할 수 있는 아이디입니다",...U,ref:f,type:"underline"})]})})]})},d={render:B,args:{lazy:!1,required:!0}},u={render:B,args:{lazy:!0,required:!0}};var y,j,b;o.parameters={...o.parameters,docs:{...(y=o.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: NormalTemplate,
+  args: {
+    type: 'outline',
+    rows: 4
+  }
+}`,...(b=(j=o.parameters)==null?void 0:j.docs)==null?void 0:b.source}}};var w,T,A;i.parameters={...i.parameters,docs:{...(w=i.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  render: NormalTemplate,
+  args: {
+    type: 'underline',
+    rows: 4
+  }
+}`,...(A=(T=i.parameters)==null?void 0:T.docs)==null?void 0:A.source}}};var v,C,S;d.parameters={...d.parameters,docs:{...(v=d.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: ValidationTemplate,
+  args: {
+    lazy: false,
+    required: true
+  }
+}`,...(S=(C=d.parameters)==null?void 0:C.docs)==null?void 0:S.source}}};var q,R,V;u.parameters={...u.parameters,docs:{...(q=u.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  render: ValidationTemplate,
+  args: {
+    lazy: true,
+    required: true
+  }
+}`,...(V=(R=u.parameters)==null?void 0:R.docs)==null?void 0:V.source}}};const ee=["Outline","Underline","Validation","LazyValidation"];export{u as LazyValidation,o as Outline,i as Underline,d as Validation,ee as __namedExportsOrder,$ as default};
