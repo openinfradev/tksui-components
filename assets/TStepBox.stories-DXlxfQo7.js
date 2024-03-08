aebf67ebacda2670321314f9680b7b006374bdf2
Siyeop <Siyeop@users.noreply.github.com>
2024-03-08 01:32:17 +0000

    deploy: 3fd071c1f37845e235954d4d320363af07690345

--- assets/TStepBox.stories-DXlxfQo7.js
+++
@@ -1,8 +0,0 @@
-import{j as e}from"./jsx-runtime-CKrituN3.js";import{r}from"./index-CBqU2yxZ.js";import{T as p,a as n}from"./TStepBox-DCvTsY9g.js";import"./_commonjsHelpers-BosuxZz1.js";import"./TIcon-BjO0CGSV.js";import"./TButton-B7bbPIyJ.js";import"./UseRipple-B0f24SXl.js";import"./uniqueId-BdmaqBQH.js";import"./toString-Biug5Hwg.js";const h={title:"DataContainer/TStepBox",component:p},i=u=>{const[m,l]=r.useState(1);return r.useState(""),r.useRef(null),e.jsx(e.Fragment,{children:e.jsxs(p,{value:m,onChange:l,stepLabels:["csp","step 2"],...u,children:[e.jsx(n,{nextButtonDisabled:!0,children:e.jsx("div",{children:"Step 2 Content"})}),e.jsx(n,{children:e.jsx("div",{children:"Step 2 Content"})})]})})},t={render:i,args:{prevButtonLabel:"이전",nextButtonLabel:"다음",completeButtonLabel:"저장하기"}};var o,s,a;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  render: Template,
-  args: {
-    prevButtonLabel: '이전',
-    nextButtonLabel: '다음',
-    completeButtonLabel: '저장하기'
-  }
-}`,...(a=(s=t.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const L=["Default"];export{t as Default,L as __namedExportsOrder,h as default};
