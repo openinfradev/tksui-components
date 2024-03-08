442992418feb39847a48dba5911b618b2ec42199
kkusaeng <macjjuni@strato.co.kr>
2024-03-08 14:57:24 +0900

    feature. Add new custom icon
    - t_bullet

--- src/components/icon/TIconOriginal.tsx
+++ src/components/icon/TIconOriginal.tsx
@@ -152,6 +152,11 @@ const TOriginalImage = {
             <rect x='6.41992' y='9.41992' width='7' height='1'/>
         </svg>
     ),
+    t_bullet: (
+        <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 20 20'>
+            <circle cx='10' cy='10' r='2' />
+        </svg>
+    ),
 };
 
 export default TOriginalImage;
