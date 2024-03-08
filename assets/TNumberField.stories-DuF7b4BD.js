4f4fc6468987d9b3116052dadf689a21c0f3725f
Siyeop <Siyeop@users.noreply.github.com>
2024-03-08 05:46:31 +0000

    deploy: 5fa72f7b175457871093d5953cca51febd9d4b9d

---
+++ assets/TNumberField.stories-DuF7b4BD.js
@@ -0,0 +1,23 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{r as x}from"./TValidatorRule-BwpR5Vl5.js";import{u as n}from"./UseInputState-BMLhFmM_.js";import{T as O}from"./TButton-Bzqw7imh.js";import{u as C}from"./UseRefs-B71Mt8Xu.js";import{T as t}from"./TNumberField-BuLgJdu3.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./UseRipple-B0f24SXl.js";import"./uniqueId-BdmaqBQH.js";import"./toString-Biug5Hwg.js";import"./TIcon-CuhwYyyf.js";import"./UseValidator-XS2alT9g.js";const M={title:"Input/TNumberField",component:t},z=r=>{const p=n(""),m=n(""),a=n(""),i=n(""),u=n(""),c=n(""),w=n(""),R={width:"500px",border:"1px solid lightgray",padding:"16px",marginTop:"16px",display:"flex",flexDirection:"column",gap:"16px"};return e.jsx(e.Fragment,{children:e.jsx("div",{style:{display:"flex",gap:"24px"},children:e.jsxs("div",{style:R,children:[e.jsx(t,{...r,label:"값 제한 없음",...p}),e.jsx(t,{...r,label:"0-10, step 1",...m,min:0,max:10,step:1}),e.jsx(t,{...r,label:"3-99, step 3",...a,min:3,max:99,step:3}),e.jsx(t,{...r,label:"-10-10, step 5",...i,min:-10,max:10,step:5}),e.jsx(t,{...r,label:"읽기 전용",value:"3",disabled:!0}),e.jsx(t,{...r,label:"커스텀 너비",...u,width:"200px"}),e.jsx(t,{...r,label:"필수 값",...c,required:!0}),e.jsx(t,{...r,label:"가이드 메시지",...w,width:"400px",hint:"개수를 줄이시면 가장 마지막 호스트네임부터 삭제 됩니다."})]})})})},s={render:z,args:{type:"outline"}},l={render:z,args:{type:"underline"}},N=r=>{const p=n(""),m=n(""),[a,i]=C(2),u={width:"500px",border:"1px solid lightgray",padding:"16px",marginTop:"16px",display:"flex",flexDirection:"column",gap:"16px"},c=()=>{a.current.validate(),i.current.validate()};return e.jsxs(e.Fragment,{children:[e.jsx(O,{main:!0,onClick:c,children:"검사"}),e.jsx("div",{style:{display:"flex",gap:"24px"},children:e.jsxs("div",{style:u,children:[e.jsx(t,{...r,rules:[x.required()],...p,ref:a,required:!0,label:"필수 값"}),e.jsx(t,{...r,rules:[x.required()],...m,ref:i,required:!0,type:"underline",label:"필수 값",hint:"개수를 줄이시면 가장 마지막 호스트네임부터 삭제 됩니다."})]})})]})},o={render:N,args:{lazy:!1,required:!0}},d={render:N,args:{lazy:!0,required:!0}};var y,b,f;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: NormalTemplate,
+  args: {
+    type: 'outline'
+  }
+}`,...(f=(b=s.parameters)==null?void 0:b.docs)==null?void 0:f.source}}};var j,g,h;l.parameters={...l.parameters,docs:{...(j=l.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: NormalTemplate,
+  args: {
+    type: 'underline'
+  }
+}`,...(h=(g=l.parameters)==null?void 0:g.docs)==null?void 0:h.source}}};var F,T,q;o.parameters={...o.parameters,docs:{...(F=o.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  render: ValidationTemplate,
+  args: {
+    lazy: false,
+    required: true
+  }
+}`,...(q=(T=o.parameters)==null?void 0:T.docs)==null?void 0:q.source}}};var v,S,V;d.parameters={...d.parameters,docs:{...(v=d.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: ValidationTemplate,
+  args: {
+    lazy: true,
+    required: true
+  }
+}`,...(V=(S=d.parameters)==null?void 0:S.docs)==null?void 0:V.source}}};const P=["Outline","Underline","Validation","LazyValidation"];export{d as LazyValidation,s as Outline,l as Underline,o as Validation,P as __namedExportsOrder,M as default};
---
+++ assets/TPage.stories-n1NVa9sx.js
@@ -0,0 +1,25 @@
+import{j as e}from"./jsx-runtime-CKrituN3.js";import{T as m}from"./TPage-BVThT5cJ.js";import{T as t}from"./TSection-IZqRhGi2.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./TIcon-CuhwYyyf.js";const T={title:"Screen/TPage",component:m},u=`
+Lorem ipsum dolor sit amet, consec
+tetur adipiscing elit. In nec consecte
+tur justo, in faucibus nisl. Donec acc
+umsan, sem non pellentesque ullam
+corper, diam lectus cursus urna, 
+in euismod dui risus vitae massa. 
+Donec pretium faucibus placerat. 
+Nulla facilisi. Proin ultricies, 
+nulla eget efficitur facilisis, orci quam acc
+umsan nulla, eget condimentum pur
+us massa eu velit. Suspendisse fring
+illa ex ex, in facilisis urna sagittis vel. Cras sit amet mattis urna. Etiam vehi
+cula cursus lorem ac tempor.`,x=j=>e.jsx(e.Fragment,{children:e.jsxs(m,{title:"클러스터 관리",...j,children:[e.jsx(t,{label:"컨텐츠 1",children:e.jsxs("p",{children:["여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{})]})}),e.jsx(t,{label:"컨텐츠 2",children:e.jsxs("p",{children:["여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",e.jsx("br",{})]})})]})}),r={render:x,args:{infoPanelContent:u}},s={render:x,args:{infoPanelContent:u,contentDirection:"left-right"}};var n,i,a;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    infoPanelContent: infoContent
+  }
+}`,...(a=(i=r.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};var o,c,l;s.parameters={...s.parameters,docs:{...(o=s.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    infoPanelContent: infoContent,
+    contentDirection: 'left-right'
+  }
+}`,...(l=(c=s.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};const C=["TopBottom","LeftRight"];export{s as LeftRight,r as TopBottom,C as __namedExportsOrder,T as default};
