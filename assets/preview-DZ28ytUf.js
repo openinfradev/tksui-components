c1ed68eabea50e3e4e1247d8202086850a9aa86d
Siyeop <Siyeop@users.noreply.github.com>
2024-03-21 07:03:51 +0000

    deploy: 8ccc24426991d50b7e2f247bd22bece3ad0774d4

--- assets/preview-DZ28ytUf.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-C8Br3fI9.js","./iframe-D9P18NxS.js","./index-CBqU2yxZ.js","./_commonjsHelpers-BosuxZz1.js","./react-18-D8cruF67.js","./index-BtM5VmRH.js","./index-B_J8iUie.js","./toString-Biug5Hwg.js","./_getPrototype-BHeJXxZI.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as t}from"./iframe-D9P18NxS.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-C8Br3fI9.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{_ as parameters};
