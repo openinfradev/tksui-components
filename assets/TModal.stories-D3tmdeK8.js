1b9c38926c5856a6e7ba9c654a5f08bbd29af4f8
kkusaeng <kkusaeng@users.noreply.github.com>
2024-04-16 23:54:06 +0000

    deploy: 27e5756a63200384908d04a18762c7909e0f92ec

---
+++ assets/TModal.stories-D3tmdeK8.js
@@ -0,0 +1,26 @@
+import{j as r}from"./jsx-runtime-CKrituN3.js";import{r as z}from"./index-CBqU2yxZ.js";import{w as k,a as p}from"./TButtonGroup-C_vQbSYX.js";import"./_commonjsHelpers-BosuxZz1.js";import"./TIconButton-DWbAcNt8.js";import"./UseRipple-B0f24SXl.js";import"./uniqueId-BdmaqBQH.js";import"./toString-Biug5Hwg.js";import"./TIcon-BoQRjeXH.js";import"./ThemeToken.module-BX06fUfM.js";import"./index-Dk74W0Oi.js";import"./index-BtM5VmRH.js";import"./TBadge-C5SG4UU5.js";import"./UseValidator-XS2alT9g.js";/* empty css                      */import"./TDropHolder-B8JchZc7.js";import"./TPage-CVLraPgO.js";import"./TTooltip-hEd-iOmd.js";import"./THighlightText-ySiKuYph.js";import"./TTabItem-BNADMDmB.js";import"./TCardContent-C4rv2K10.js";import"./TInputValidationHint-B6k-5azr.js";import"./TChip-DZDFK5dv.js";import"./TTextField-Crckvu5f.js";import"./TSwitch-De2qQJKg.js";const rr={title:"Screen/TModal",component:k,args:{containerId:"storybook-root"}},e=C=>{const[E,m]=z.useState(!1),O=r.jsxs(r.Fragment,{children:[r.jsx(p,{size:"large",onClick:()=>m(!1),children:"취소"}),r.jsx(p,{size:"large",main:!0,children:"저장"})]});return r.jsxs(r.Fragment,{children:[r.jsx(p,{onClick:()=>m(!0),children:"모달 열기"}),r.jsx(k,{...C,title:"클러스터 생성",isOpen:E,onRequestClose:()=>m(!1),footer:O,children:"클러스터 생성 모달입니다."})]})},a={render:e,args:{small:!0}},t={render:e,args:{medium:!0}},s={render:e,args:{large:!0}},o={render:e,args:{xlarge:!0}},n={render:e,args:{xxlarge:!0}};var i,c,l;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    small: true
+  }
+}`,...(l=(c=a.parameters)==null?void 0:c.docs)==null?void 0:l.source}}};var d,u,g;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    medium: true
+  }
+}`,...(g=(u=t.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var x,S,T;s.parameters={...s.parameters,docs:{...(x=s.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    large: true
+  }
+}`,...(T=(S=s.parameters)==null?void 0:S.docs)==null?void 0:T.source}}};var f,j,h;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    xlarge: true
+  }
+}`,...(h=(j=o.parameters)==null?void 0:j.docs)==null?void 0:h.source}}};var L,X,M;n.parameters={...n.parameters,docs:{...(L=n.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    xxlarge: true
+  }
+}`,...(M=(X=n.parameters)==null?void 0:X.docs)==null?void 0:M.source}}};const er=["Small","Medium","Large","XLarge","XXLarge"];export{s as Large,t as Medium,a as Small,o as XLarge,n as XXLarge,er as __namedExportsOrder,rr as default};
