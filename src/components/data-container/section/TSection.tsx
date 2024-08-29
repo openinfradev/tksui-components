05769271f7eb56ccee5fabb8f7fd3288fa08be4b
Siyeop <yeop22@naver.com>
2024-08-29 11:40:23 +0900

    enhancement. Memoization on components - 2

--- src/components/data-container/section/TSection.tsx
+++ src/components/data-container/section/TSection.tsx
@@ -1,4 +1,4 @@
-import {CSSProperties, useMemo} from 'react';
+import {CSSProperties, memo, useMemo} from 'react';
 import {TSectionProps} from './TSection.interface';
 
 function TSection(props: TSectionProps) {
@@ -56,4 +56,4 @@ function TSection(props: TSectionProps) {
 
 }
 
-export default TSection;
+export default memo(TSection);
