74cf4d36b3c14cd9b0a9f44814289febc535dd47
Siyeop <yeop22@naver.com>
2024-09-05 17:14:20 +0900

    feature. Add new component TForm

---
+++ src/components/data-container/form/TFormRow.tsx
@@ -0,0 +1,51 @@
+import {CSSProperties} from 'react';
+import TFormColumn from './TFormColumn';
+import {TFormColumnProps, TFormRowProps} from '~/data-container/form/TForm.interface';
+
+const TFormRow = ({
+    labelVerticalAlign,
+    ...restProps
+}: TFormRowProps) => {
+
+    const props: TFormRowProps = {labelVerticalAlign, ...restProps};
+
+
+    const rootClass = (): string => {
+        const clazz: string[] = [];
+
+        if (props.className) { clazz.push(props.className); }
+
+        return clazz.join(' ');
+    };
+
+    const rootStyle: CSSProperties = props.style ? props.style : null;
+
+    const formColumn = (columnProps: TFormColumnProps) => TFormColumn(columnProps);
+
+    const items = props.columns.map((item, index) => {
+
+        return (
+            formColumn({
+                ...item,
+                key: `${props.key}${index}`,
+                column: props.column,
+                labelWidth: props.labelWidth,
+                labelVerticalAlign,
+            })
+        );
+    });
+
+
+    return (
+        <div
+            className={`t-form-row ${rootClass()}`}
+            style={rootStyle}
+            key={props.key}
+        >
+            {items}
+        </div>
+    );
+
+};
+
+export default TFormRow;
---
+++ src/components/data-container/form/index.ts
@@ -0,0 +1,5 @@
+export {default as TForm} from './TForm';
+export {default as TFormRow} from './TFormRow';
+export {default as TFormItem} from './TFormColumn';
+
+export * from './TForm.interface';
