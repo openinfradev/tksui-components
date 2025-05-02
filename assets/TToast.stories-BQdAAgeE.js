3a07c84517642bbcbd619bcc15a39355aba2534d
Siyeop <Siyeop@users.noreply.github.com>
2025-05-02 10:00:42 +0000

    deploy: 711b654a4413bb1fc774aad7a3f906b2bac02396

---
+++ assets/TToast.stories-BQdAAgeE.js
@@ -0,0 +1,10 @@
+import{j as o}from"./jsx-runtime-Ds-gkUgj.js";import{a as s}from"./TButtonGroup-DYDPUbYL.js";import{T as j,n as e}from"./TChip-C9xxd8Xw.js";import"./index-CtvPRVHf.js";import"./TTabItem-D-URtI3h.js";import"./TIcon-CZXMjSet.js";import"./index-W1kuYZ3C.js";import"./TBadge-B9MxuGag.js";import"./UseValidator-BAGPP7kt.js";import"./TDropHolder-BLUtAzpi.js";import"./THighlightText-DDFwjSf2.js";import"./TInputValidationHint-ClT4UQcI.js";import"./TTextField-BTej2wNt.js";import"./TSwitch-ColYbz3N.js";const W={title:"Guide/TToast",component:j},f=h=>{const t="Hello world",n="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry";function a(r){e(r)}function c(r){e.success(r)}function l(r){e.error(r)}function m(r){e.warn(r)}function p(r){e.info(r)}return o.jsxs(o.Fragment,{children:[o.jsx(s,{onClick:()=>a(t),children:"Plain short"}),o.jsx(s,{onClick:()=>a(n),children:"Plain long"}),o.jsx("br",{}),o.jsx("br",{}),o.jsx(s,{onClick:()=>c(t),children:"Success short"}),o.jsx(s,{onClick:()=>c(n),children:"Success long"}),o.jsx("br",{}),o.jsx("br",{}),o.jsx(s,{onClick:()=>l(t),children:"Error short"}),o.jsx(s,{onClick:()=>l(n),children:"Error long"}),o.jsx("br",{}),o.jsx("br",{}),o.jsx(s,{onClick:()=>m(t),children:"Warn short"}),o.jsx(s,{onClick:()=>m(n),children:"Warn long"}),o.jsx("br",{}),o.jsx("br",{}),o.jsx(s,{onClick:()=>p(t),children:"Info short"}),o.jsx(s,{onClick:()=>p(n),children:"Info long"}),o.jsx("br",{}),o.jsx("br",{}),o.jsx(j,{...h})]})},i={render:f,args:{className:"tks-toast-container",autoClose:1e4,toastClassName:"tks-toast",position:"top-right",draggable:!1}};var x,d,u;i.parameters={...i.parameters,docs:{...(x=i.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    className: 'tks-toast-container',
+    autoClose: 10000,
+    toastClassName: 'tks-toast',
+    position: 'top-right' as ToastPosition,
+    draggable: false
+  }
+}`,...(u=(d=i.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};const _=["Default"];export{i as Default,_ as __namedExportsOrder,W as default};
