7d68532cc3ba454b7925bf23f359fb08c6fb45c6
Siyeop <Siyeop@users.noreply.github.com>
2025-06-27 01:23:48 +0000

    deploy: 0ee81b64174242cfde14cafc1b3c99b0585c1495

---
+++ assets/TIconButton.stories-zINtwJoT.js
@@ -0,0 +1,14 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{b as o,c as x}from"./TButtonGroup-C-mJGCKi.js";import"./index-75N07mRN.js";import"./TTabItem-DPqjR3tx.js";import"./TIcon-DLBXrsGZ.js";import"./index-BJirbR3n.js";import"./index-Bj-N6E0A.js";import"./TBadge-B58EEO3d.js";import"./UseValidator-BSroX4ym.js";import"./TDropHolder-CeUkRMWI.js";import"./THighlightText-pTj3K_4-.js";import"./TInputValidationHint-DqK32PRs.js";import"./TChip-KzMSgKKz.js";import"./TTextField-Cw9y-yB4.js";import"./TSwitch-DoTj_LFj.js";const w={title:"Button/TIconButton",component:o},n=t=>e.jsx("div",{style:{display:"flex",flexDirection:"column",fontSize:"14px",gap:"8px",alignItems:"center"},children:t.children}),m=t=>e.jsx(e.Fragment,{children:e.jsxs("div",{style:{display:"grid",gridTemplateColumns:"repeat(4, 150px)",marginTop:"32px",rowGap:"40px",alignItems:"center",textAlign:"center"},children:[e.jsx(x,{id:"icon-button-tooltip"}),e.jsx("div",{}),e.jsx("div",{children:"Elevation"}),e.jsx("div",{children:"Flat"}),e.jsx("div",{children:"None"}),e.jsx("div",{children:"Rectangle"}),e.jsx(n,{children:e.jsx(o,{...t,shape:"rectangle",outline:"elevation",children:"add"})}),e.jsx(n,{children:e.jsx(o,{...t,shape:"rectangle",outline:"flat",children:"add"})}),e.jsx(n,{children:e.jsx(o,{...t,shape:"rectangle",outline:"none",children:"add"})}),e.jsx("div",{children:"Circle"}),e.jsx(n,{children:e.jsx(o,{...t,shape:"circle",outline:"elevation",children:"add"})}),e.jsx(n,{children:e.jsx(o,{...t,shape:"circle",outline:"flat",children:"add"})}),e.jsx(n,{children:e.jsx(o,{...t,shape:"circle",outline:"none",children:"add"})})]})}),i={render:m,args:{tooltipTitle:"title",tooltipContent:"content",tooltipPlace:"top",tooltipId:"icon-button-tooltip"}},r={render:m,args:{disabled:!0}};var l,s,a;i.parameters={...i.parameters,docs:{...(l=i.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    tooltipTitle: 'title',
+    tooltipContent: 'content',
+    tooltipPlace: 'top',
+    tooltipId: 'icon-button-tooltip'
+  }
+}`,...(a=(s=i.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};var c,d,p;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    disabled: true
+  }
+}`,...(p=(d=r.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};const F=["Default","Disabled"];export{i as Default,r as Disabled,F as __namedExportsOrder,w as default};
