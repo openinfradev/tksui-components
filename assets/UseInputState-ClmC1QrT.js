fadc903511361248a3c3b1eea03e7e173750415d
Siyeop <Siyeop@users.noreply.github.com>
2025-08-04 07:27:44 +0000

    deploy: bd3e64e3c4a847c2cd00889866c7c144a68655d7

---
+++ assets/UseInputState-ClmC1QrT.js
@@ -0,0 +1,1 @@
+import{r}from"./index-Bkkj9pTw.js";function u(e){const[a,t]=r.useState(e);function n(o){t(o)}return{value:a,onChange:n,setState:t}}export{u};
---
+++ assets/index-BBisz5Cx.js
@@ -0,0 +1,1 @@
+import{g as r}from"./index-Bkkj9pTw.js";import{r as o}from"./index-B7RxxPJE.js";var t=o();const m=r(t);export{m as I,t as r};
