18e3a4969b8d48c8deb9cabbd04e0d643e144a18
Siyeop <Siyeop@users.noreply.github.com>
2025-10-22 02:41:08 +0000

    deploy: 4b3c0551e10083d69da8aa23a6e905271bc6f274

---
+++ assets/TModal.stories-CPHYDOD1.js
@@ -0,0 +1,26 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{r as y}from"./index-BqztxKQk.js";import{A as M,R as C,a as p}from"./TButtonGroup-DkzWU4CD.js";import"./TTabItem-CKcer2mH.js";import"./TIcon-CYhWV6Ep.js";import"./index-DmlQtXDA.js";import"./index-C6xMosk4.js";import"./TBadge-B8yc9zRE.js";import"./UseValidator-DgLpkHEc.js";import"./TDropHolder-B7z40qzo.js";import"./THighlightText-pTj3K_4-.js";import"./TInputValidationHint-C1mvkrXU.js";import"./TChip-DgOBpbU4.js";import"./TTextField-C6MSjj1d.js";import"./TSwitch-YoEgX14L.js";const N={title:"Screen/TModal",component:M,args:{appId:"storybook-root",portalId:"storybook-root"}},e=E=>{const[R,m]=y.useState(!1),b=r.jsxs(r.Fragment,{children:[r.jsx(p,{size:"large",onClick:()=>m(!1),children:"취소"}),r.jsx(p,{size:"large",main:!0,children:"저장"})]});return C.setAppElement("#storybook-root"),r.jsxs(r.Fragment,{children:[r.jsx("div",{id:"test-test"}),r.jsx(p,{onClick:()=>m(!0),children:"모달 열기"}),r.jsx(M,{...E,title:"클러스터 생성",isOpen:R,onRequestClose:()=>m(!1),footer:b,children:"클러스터 생성 모달입니다."})]})},a={render:e,args:{small:!0}},s={render:e,args:{medium:!0}},t={render:e,args:{large:!0}},o={render:e,args:{xlarge:!0}},n={render:e,args:{xxlarge:!0}};var c,l,i;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    small: true
+  }
+}`,...(i=(l=a.parameters)==null?void 0:l.docs)==null?void 0:i.source}}};var d,u,g;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    medium: true
+  }
+}`,...(g=(u=s.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var x,j,S;t.parameters={...t.parameters,docs:{...(x=t.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    large: true
+  }
+}`,...(S=(j=t.parameters)==null?void 0:j.docs)==null?void 0:S.source}}};var T,f,h;o.parameters={...o.parameters,docs:{...(T=o.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    xlarge: true
+  }
+}`,...(h=(f=o.parameters)==null?void 0:f.docs)==null?void 0:h.source}}};var L,X,k;n.parameters={...n.parameters,docs:{...(L=n.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    xxlarge: true
+  }
+}`,...(k=(X=n.parameters)==null?void 0:X.docs)==null?void 0:k.source}}};const P=["Small","Medium","Large","XLarge","XXLarge"];export{t as Large,s as Medium,a as Small,o as XLarge,n as XXLarge,P as __namedExportsOrder,N as default};
---
+++ assets/TPage.stories-BCHHcNKF.js
@@ -0,0 +1,26 @@
+import{j as r}from"./jsx-runtime-D_zvdyIk.js";import{B as l,o as t}from"./TButtonGroup-DkzWU4CD.js";import"./index-BqztxKQk.js";import"./TTabItem-CKcer2mH.js";import"./TIcon-CYhWV6Ep.js";import"./index-DmlQtXDA.js";import"./index-C6xMosk4.js";import"./TBadge-B8yc9zRE.js";import"./UseValidator-DgLpkHEc.js";import"./TDropHolder-B7z40qzo.js";import"./THighlightText-pTj3K_4-.js";import"./TInputValidationHint-C1mvkrXU.js";import"./TChip-DgOBpbU4.js";import"./TTextField-C6MSjj1d.js";import"./TSwitch-YoEgX14L.js";const q={title:"Screen/TPage",component:l},j="Lorem ipsum dolor sit amet",u=`
+Lorem ipsum dolor sit amet, consec
+tetur adipiscing elit. In nec consecte
+tur justo, in faucibus nisl. Donec acc
+umsan, sem non pellentesque ullam
+corper, diam lectus cursus urna, 
+in euismod dui risus vitae massa. 
+Donec pretium faucibus placerat. 
+Nulla facilisi. Proin ultricies, 
+nulla eget efficitur facilisis, orci quam acc
+umsan nulla, eget condimentum pur
+us massa eu velit. Suspendisse fring
+illa ex ex, in facilisis urna sagittis vel. Cras sit amet mattis urna. Etiam vehi
+cula cursus lorem ac tempor.`,p=x=>r.jsx(r.Fragment,{children:r.jsxs(l,{title:"클러스터 관리",breadcrumb:"설정 > 클러스터 > 목록",...x,children:[r.jsx(t,{label:"컨텐츠 1",children:r.jsxs("p",{children:["여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{})]})}),r.jsx(t,{label:"컨텐츠 2",children:r.jsxs("p",{children:["여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{}),"여기에 컨텐츠를 구현합니다. ",r.jsx("br",{})]})})]})}),e={render:p,args:{infoPanelTitle:j,infoPanelContent:u}},s={render:p,args:{infoPanelContent:u,contentDirection:"left-right"}};var n,i,o;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    infoPanelTitle,
+    infoPanelContent: infoContent
+  }
+}`,...(o=(i=e.parameters)==null?void 0:i.docs)==null?void 0:o.source}}};var a,c,m;s.parameters={...s.parameters,docs:{...(a=s.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    infoPanelContent: infoContent,
+    contentDirection: 'left-right'
+  }
+}`,...(m=(c=s.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const _=["TopBottom","LeftRight"];export{s as LeftRight,e as TopBottom,_ as __namedExportsOrder,q as default};
